fe73ba819821207db23719c3b7610056d6b845ba
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-12 11:58:51 +0530

    check email

--- src/main/java/lk/jiat/bcd/controller/ProfileController.java
+++ src/main/java/lk/jiat/bcd/controller/ProfileController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -13,6 +14,12 @@ public class ProfileController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
 
+        if (session.getAttribute("email") != null){
+
+        } else {
+            resp.sendRedirect("signin.jsp");
+        }
     }
 }
