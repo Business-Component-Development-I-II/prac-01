506f8ddeb6f2bca01ae8e470670001eb45ee5b79
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-12 11:53:19 +0530

    create profilecontroller

--- src/main/java/lk/jiat/bcd/controller/SignInController.java
+++ src/main/java/lk/jiat/bcd/controller/SignInController.java
@@ -3,6 +3,7 @@ package lk.jiat.bcd.controller;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.jiat.bcd.model.User;
@@ -11,7 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @WebServlet("/signIn")
-public class SignInController {
+public class SignInController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
